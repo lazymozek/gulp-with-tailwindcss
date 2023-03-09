7d7109010b5b122fbdd45f5c71545e5bfacbdc4c
Manjunath G <manjumjng2@gmail.com>
2023-03-09 16:09:29 +0530

    Improve config

--- config.js
+++ config.js
@@ -1,33 +1,35 @@
+const config = {
+  tailwindjs: "./tailwind.config.js",
+  port: 9050,
+};
+
+// tailwind plugins
+const plugins = {
+  typograpy: true,
+  forms: true,
+  lineClamp: true,
+  containerQueries: true,
+};
+
+// base folder paths
+const basePaths = ["src", "dist", "build"];
+
+// folder assets paths
+const folders = ["css", "js", "img", "fonts", "other"];
+
+const paths = {
+  root: "./",
+};
+
+basePaths.forEach((base) => {
+  paths[base] = {
+    base: `./${base}`,
+  };
+  folders.forEach((folder) => (paths[base][folder] = `./${base}/${folder}`));
+});
+
 module.exports = {
-  config: {
-    tailwindjs: "./tailwind.config.js",
-    port: 9050,
-  },
-  plugins: {
-    typograpy: true,
-    forms: true,
-    lineClamp: true,
-    containerQueries: true,
-  },
-  paths: {
-    root: "./",
-    src: {
-      base: "./src",
-      css: "./src/css",
-      js: "./src/js",
-      img: "./src/img",
-    },
-    dist: {
-      base: "./dist",
-      css: "./dist/css",
-      js: "./dist/js",
-      img: "./dist/img",
-    },
-    build: {
-      base: "./build",
-      css: "./build/css",
-      js: "./build/js",
-      img: "./build/img",
-    },
-  },
+  config,
+  plugins,
+  paths,
 };
